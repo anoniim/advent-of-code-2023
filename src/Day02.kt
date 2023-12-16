d22f02fc895d7c8abca9f31cb783018728880444
Marcel Bro <marcel.broz@gmail.com>
2023-12-16 13:53:30 +0000

    Day 2 part 1 completed
    Signed-off-by: Marcel Bro <marcel.broz@gmail.com>

---
+++ src/Day02.kt
@@ -0,0 +1,64 @@
+fun main() {
+    Day2().run(
+        8,
+        -1
+    )
+}
+
+private class Day2 : Day(2) {
+
+    override fun part1(input: List<String>): Int {
+        // Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+        val requiredRed = 12
+        val requiredGreen = 13
+        val requiredBlue = 14
+        var sum = 0
+        input.map(Game::fromInput).forEach { game ->
+            if (game.sets.containAtLeast(requiredRed, requiredGreen, requiredBlue)) sum += game.id
+        }
+        return sum
+    }
+
+    private fun List<CubeSet>.containAtLeast(requiredRed: Int, requiredGreen: Int, requiredBlue: Int): Boolean {
+        return all { set ->
+            requiredRed >= set.red &&
+            requiredGreen >= set.green &&
+            requiredBlue >= set.blue
+        }
+    }
+
+    override fun part2(input: List<String>): Int {
+        //
+        return -1
+    }
+}
+
+private class Game (
+    val id: Int,
+    val sets: List<CubeSet>
+) {
+    companion object {
+        fun fromInput(input: String) : Game {
+            val gameSplit = input.split(":")
+            val gameId = gameSplit.first().split(" ").last().toInt()
+            val sets = gameSplit.last().split(";").map { cubeSetString ->
+                var red = 0
+                var green = 0
+                var blue = 0
+                cubeSetString.split(",").map(String::trim).forEach { cubeColorText ->
+                    if (cubeColorText.contains("red")) red = cubeColorText.split(" ").first().toInt()
+                    if (cubeColorText.contains("green")) green = cubeColorText.split(" ").first().toInt()
+                    if (cubeColorText.contains("blue")) blue = cubeColorText.split(" ").first().toInt()
+                }
+                CubeSet(red, green, blue)
+            }
+            return Game(gameId, sets)
+        }
+    }
+}
+
+private data class CubeSet(
+    val red: Int = 0,
+    val green: Int = 0,
+    val blue: Int = 0,
+)
