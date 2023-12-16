eaea93c57c97fadea0978e9051e0482ce2bfabb2
Marcel Bro <marcel.broz@gmail.com>
2023-12-16 17:05:45 +0000

    Day 3 part 2 completed
    Signed-off-by: Marcel Bro <marcel.broz@gmail.com>

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,9 +1,7 @@
-import kotlin.time.Duration.Companion.seconds
-
 fun main() {
     Day3().run(
         4361,
-        -1
+        467835
     )
 }
 
@@ -12,51 +10,68 @@ private class Day3 : Day(3) {
     override fun part1(input: List<String>): Int {
         // What is the sum of all of the part numbers in the engine schematic?
         val engine = EngineSchematic.fromInput(input)
-        return engine.getPartNumbers().sumOf { it.first }
+        return engine.getPartNumbers().sumOf { it.number }
     }
 
     override fun part2(input: List<String>): Int {
-        //
-        return -1
+        // What is the sum of all of the gear ratios in your engine schematic?
+        val engine = EngineSchematic.fromInput(input)
+        return engine.getGearRatios().sum()
     }
 }
 
-typealias PartNumbers = List<Pair<Int, IntRange>> // Pair<number, range of indices>
-typealias Symbols = List<Int> // List of indices
+class PartNumber(val number: Int, val indexRange: IntRange)
+class Symbol(val char: Char, val index: Int)
 
 private class EngineSchematic(
     private val width: Int,
-    private val allNumbers: PartNumbers,
-    private val allSymbols: Symbols,
+    private val allNumbers: List<PartNumber>,
+    private val allSymbols: List<Symbol>,
 ) {
 
-    fun getPartNumbers(): PartNumbers {
+    fun getPartNumbers(): List<PartNumber> {
         return allNumbers.filter {
-            val startIndex = it.second.first
-            val endIndex = it.second.last
+            val startIndex = it.indexRange.first
+            val endIndex = it.indexRange.last
             startIndex.isAdjacentToSymbol() || endIndex.isAdjacentToSymbol()
         }
     }
 
+    fun getGearRatios(): List<Int> {
+        return allSymbols.filter { it.char == '*' }
+            .map { symbol ->
+                val adjacentPartNumbers = mutableListOf<Int>()
+                allNumbers.forEach { partNumber ->
+                    if (symbol.index.isAdjacentTo(partNumber.indexRange.first) || symbol.index.isAdjacentTo(partNumber.indexRange.last)) {
+                        adjacentPartNumbers.add(partNumber.number)
+                    }
+                }
+                // Return gear ratios for gears that only have 2 neighbors
+                if (adjacentPartNumbers.size == 2) adjacentPartNumbers[0] * adjacentPartNumbers[1] else 0
+            }
+    }
+
     private fun Int.isAdjacentToSymbol(): Boolean {
         // Is this number adjacent to any symbol?
-        return allSymbols.any { symbolIndex ->
-            (symbolIndex - width) == this || (symbolIndex + width) == this ||
-                    (if (this.isNotAtTheBeginningOfLine()) {
-                        (symbolIndex - width - 1) == this || (symbolIndex - 1) == this || (symbolIndex + width - 1) == this
-                    } else false) ||
-                    (if (this.isNotAtTheEndOfLine()) {
-                        (symbolIndex - width + 1) == this || (symbolIndex + 1) == this || (symbolIndex + width + 1) == this
-                    } else false)
+        return allSymbols.any { symbol ->
+            isAdjacentTo(symbol.index)
         }
     }
+
+    private fun Int.isAdjacentTo(otherIndex: Int) = (otherIndex - width) == this || (otherIndex + width) == this ||
+            (if (this.isNotAtTheBeginningOfLine()) {
+                (otherIndex - width - 1) == this || (otherIndex - 1) == this || (otherIndex + width - 1) == this
+            } else false) ||
+            (if (this.isNotAtTheEndOfLine()) {
+                (otherIndex - width + 1) == this || (otherIndex + 1) == this || (otherIndex + width + 1) == this
+            } else false)
 
     private fun Int.isNotAtTheBeginningOfLine(): Boolean {
         return this % width != 0
     }
 
     private fun Int.isNotAtTheEndOfLine(): Boolean {
-        return this % width != width-1
+        return this % width != width - 1
     }
 
     companion object {
@@ -66,11 +81,11 @@ private class EngineSchematic(
             val numberRegex = Regex("""(\d)+""")
             val matchResults = numberRegex.findAll(fullInput)
             val numbers = matchResults.map { matchResult ->
-                Pair(matchResult.value.toInt(), matchResult.range)
+                PartNumber(matchResult.value.toInt(), matchResult.range)
             }.toList()
 
             val symbols = fullInput.mapIndexed { index, char ->
-                if (!char.isDigit() && char != '.') index else null
+                if (!char.isDigit() && char != '.') Symbol(char, index) else null
             }.filterNotNull()
             return EngineSchematic(width, numbers, symbols)
         }
