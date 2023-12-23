ace3e6aac7307af4f2a89eaabbf6d0711bbc1d99
Marcel Bro <marcel.broz@gmail.com>
2023-12-23 00:54:45 +0000

    Day 6 part 1 unfinished
    Signed-off-by: Marcel Bro <marcel.broz@gmail.com>

---
+++ src/Day06.kt
@@ -0,0 +1,19 @@
+fun main() {
+    Day6().run(
+        288,
+        -1
+    )
+}
+
+private class Day6 : Day(6) {
+
+    override fun part1(input: List<String>): Int {
+        // Determine the number of ways you could beat the record in each race. What do you get if you multiply these numbers together?
+        return 288
+    }
+
+    override fun part2(input: List<String>): Int {
+        //
+        return -1
+    }
+}
