14abe8c61ad3cab6417b8696929c7093dc9e2dbe
Marcel Bro <marcel.broz@gmail.com>
2024-01-12 16:42:07 +0000

    Day 9 part 1 unfinished

---
+++ src/Day09.kt
@@ -0,0 +1,50 @@
+import jdk.internal.org.jline.reader.History
+
+fun main() {
+    Day9().run(
+        114,
+        -1
+    )
+}
+
+private class Day9 : Day(9) {
+
+    override fun part1(input: List<String>): Int {
+        // Analyze your OASIS report and extrapolate the next value for each history. What is the sum of these extrapolated values?
+        return input.map(History::fromInput)
+            .sumOf(History::getPrediction)
+    }
+
+    override fun part2(input: List<String>): Int {
+        //
+        return -1
+    }
+
+    private class History(val values: List<Int>) {
+
+        fun getPrediction(): Int {
+            val steps = calculateSteps()
+
+        }
+
+        private fun calculateSteps(): MutableList<Int> {
+            val steps = mutableListOf<Int>()
+            var nextStep = values
+            do {
+                nextStep = nextStep.getNextStep()
+                steps.add(nextStep.last())
+            } while (nextStep.all { it == 0 })
+            return steps
+        }
+
+        private fun List<Int>.getNextStep(): List<Int> {
+
+        }
+
+        companion object {
+            fun fromInput(input: String): History {
+                return History(input.split(" ").map(String::toInt))
+            }
+        }
+    }
+}
